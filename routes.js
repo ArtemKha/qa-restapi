02a71f0ffa6529387d8b1f103484263a3ec5c9b0
artemkha <khaapt@gmail.com>
2017-09-02 19:43:06 +0300

    rest api

---
+++ routes.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const router = express.Router()
+
+// GET /questions
+router.get('/', (req,res) => {
+  res.json({response: "You sent me a GET request"})
+})
+
+// POST /questions
+router.post('/', (req,res) => {
+  res.json({
+    response: "You sent me a POST request",
+    body: req.body 
+  })
+})
+
+// POST /questions:id
+router.get('/:id', (req,res) => {
+  res.json({
+    response: "You sent me a GET request for ID: " + req.params.id 
+  })
+})
+
+
+// POST /questions/:id/answers
+router.post('/:qID/answers', (req, res) => {
+  res.json({
+    response: "You sent me a POST request to /answers",
+    qusetionqId: req.params.qID,
+    body: req.body
+  })
+})
+
+// POST /questions/:id/answers/:aID
+router.put('/:qID/answers/:aID', (req, res) => {
+  res.json({
+    response: "You sent me a PUT request to /answers",
+    qusetionId: req.params.qID,
+    answerId: req.params.aID,
+    body: req.body
+  })
+})
+
+// DELETE /questions/:id/answers/:aID
+router.delete('/:qID/answers/:aID', (req, res) => {
+  res.json({
+    response: "You sent me a DELETE request to /answers",
+    qusetionId: req.params.qID,
+    answerId: req.params.aID,
+  })
+})
+
+// POST /questions/:id/answers/:aID/vote-up 
+// POST /questions/:id/answers/:aID/vote-down 
+router.post('/:qID/answers/:aID/vote-:dir', (req, res) => {
+  res.json({
+    response: "You sent me a POST request to /vote-" + req.params.dir,
+    qusetionId: req.params.qID,
+    answerId: req.params.aID,
+    vote: req.params.dir,
+  })
+})
+
+
+module.exports = router
